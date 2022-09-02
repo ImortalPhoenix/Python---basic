e08b8a3d0ee2de47a53d2e5f166cadd02045daca
Arpit Manohar Dhore <arpit.dhore8@live.com>
2022-09-02 16:27:14 +0530

    new changes in python basics
    Some changes in search algorithms and sorting algorithm is added

--- graph-composer-master/graph_template.py
+++ graph-composer-master/graph_template.py
@@ -11,45 +11,58 @@ Github: https://www.github.com/kying18
 Programmer Beast Mode Spotify playlist: https://open.spotify.com/playlist/4Akns5EUb3gzmlXIdsJkPs?si=qGc4ubKRRYmPHAJAIrCxVQ 
 """
 
+from optparse import Values
 import random
 
+# define the graph in terms of vertices
 class Vertex(object):
-    def __init__(self, value):
-        pass
+    def __init__(self, value): # value will be the word
+        self.value = value
+        self.adjacent = {} # nodes that have an edge from this vertex
 
     def add_edge_to(self, vertex, weight=0):
-        pass
-
+        # this is adding an edge to the vertex we input with weight
+        self.adjacent[vertex] = weight
+        
     def increment_edge(self, vertex):
-        pass
-
+        # this is incrementing the weight of the edge
+        self.adjacent[vertex] = self.adjacent.get(vertex, 0) + 1  
+        
     def get_adjacent_nodes(self):
         pass
 
     # initializes probability map
     def get_probability_map(self):
-        pass
+        for (vertex, weight) in self.adjacent.items():
+            
 
     def next_word(self):
-        pass
+        # Randomly select next word ***Based on weights!!!
+        return random.choices(self.)
 
 
+# Now that we have our vertex representation, we put this together in a graph
 
 class Graph(object):
     def __init__(self):
-        pass
+        self.vertices = {}
 
     def get_vertex_values(self):
-        pass
-
+        # what are the values of all the verticies?
+        # in other words, return all possible words
+        return set(self.vertices.keys())
+        
     def add_vertex(self, value):
-        pass
-
+        self.vertices[value] = Vertex(value)
+         
     def get_vertex(self, value):
-        pass
+        # what if the value isnt in the graph?
+        if value not in self.vertices:
+            self.add_vertex(value)
+        return self.vertices[value] # get the Vertex object
 
     def get_next_word(self, current_vertex):
-        pass
+        self.vertices[current_vertex]
 
     def generate_probability_mappings(self):
         pass
